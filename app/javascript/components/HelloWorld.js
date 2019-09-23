708b63f612dd24d9139dc52bf565867fca625017
winescout <matt@notch8.com>
2019-09-23 18:19:28 +0000

    adds react-strap and bootstrap

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,9 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import { Alert } from 'reactstrap'
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <Alert color="primary">
+          This is a primary alert — check it out!
+        </Alert>
         <h1>HelloWorld Component</h1>
       </React.Fragment>
     );
